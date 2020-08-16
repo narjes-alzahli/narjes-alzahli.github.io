d7f6fb44fe4b6a3181e25982b822c5e3d13d95bf
Anya Ji <aj592@cornell.edu>
2020-08-15 22:25:31 -0400

    Updates

---
+++ precache-manifest.f19e22ec0dbaf49b71b0ba347cb8d382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62b05db3a324a321e4fdf307034e3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b857278098e30bc75534",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b857278098e30bc75534",
+    "url": "/static/js/main.84c8dcf0.chunk.js"
+  },
+  {
+    "revision": "53ff9637c311bd424e1b",
+    "url": "/static/js/runtime-main.5a7750ca.js"
+  }
+]);
